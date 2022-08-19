dc23b24d8a8af04cd27cbd076925e4da55b6ad4a
Евгений К <direct.ev@gmail.com>
2022-08-19 17:37:47 +0300

    v.3

---
+++ AutorizationGmailPageTests/PageObgects/MainMenuPageObgect.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutorizationGmailPageTests.PageObgects
+{
+	internal class MainMenuPageObgect
+	{
+		private IWebDriver _webDriver;
+
+		private readonly By _signInButton = By.XPath("//*[contains(@href,'GAZAmgQ')]");
+
+		public MainMenuPageObgect(IWebDriver webDriver)
+		{
+			_webDriver = webDriver;
+		}
+
+		public AuthorizationPageObject SignIn()
+		{
+			_webDriver.FindElement(_signInButton).Click();
+			return new AuthorizationPageObject(_webDriver);
+		}
+
+	}
+}
